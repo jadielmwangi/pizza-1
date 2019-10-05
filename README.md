84b16b79a13ab4ac15dc38904f26e04443da127f
margaret254 <maggiemwas91@gmail.com>
2019-10-05 23:09:01 +0300

    Add a README.md file for the project

---
+++ README.md
@@ -0,0 +1,64 @@
+
+# PIZZA HUT
+#### This project enables users to order pizzas online and even receive their deliveries at home, October 2019.
+#### By **Margaret254**&trade;
+
+## Description
+This is a pizza shop website where users can order one or even more pizzas  and also they can receive their deliveries at their homes.
+## Project live site
+  This is the live link .[ https://github.com/margaret254/pizza ]()
+
+  ![Image](/home/moringa/Documents/moringa-prep/pizza/images/web-screenshot.png)
+
+## Setup instructions
+* Clone the program from [Online repo]()
+* Click on the green button to clone or download
+
+## Behavior Driven Development
+<table>
+    <tr>
+      <th>Behavior</th> 
+      <th>Input</th> 
+      <th>Output</th>   
+    </tr>
+    <tr>
+        <td>on load</td>
+        <td>scroll and see different pizza images and names </td>
+        <td>look at the ones you like</td>
+    </tr>
+     <tr>
+        <td>on order online</td>
+        <td>click on order now or scroll down to order now and order </td>
+        <td>on checkout you will see what you have ordered and the bill to be paid</td>
+    </tr> 
+    <tr>
+        <td>Enter your name if you want to leave a message</td>
+        <td>['Serah']</td>
+        <td>name = 'Serah'</td>
+    </tr>
+    <tr>
+        <td>Enter Email && Message</td>
+        <td>['Email','Message']</td>
+        <td>Email && Message</td>
+    </tr>
+    <tr>
+        <td>Submit</td>
+        <td>Email && Message</td>
+        <td>'Hi Serah we have received your message and we will get in touch. Thank you for contacting us'</td>
+    </tr>
+       
+</table>
+
+## Technologies Used
+* HTML-For Building Mark Up pages
+* CSS-For styling User Interface
+* Bootsrap-For making webpage responsive
+* JQuery-For animations and cool effects of the website like hiding and showing elements
+* Javascript-For DOM(Document Object Manipulation)scripts
+
+## Support and contact details
+contact me https://github.com/margaret254/pizza/issues
+### License
+The project is under[MIT license](/blob/master/LICENSE)
+Copyright &copy; 2019.All rigths reserved
+  
--- js/scripts.js
+++ js/scripts.js
@@ -2,27 +2,73 @@ $('.carousel').carousel({
     interval: 2000
   })
 
-  //Business logic
-  function Order(type,size,crust,topping){
-      this.type = type;
-      this.size = size;
-      this.crust = crust;
-      this.topping = topping;
-  }
+//   //Business logic
+//   function Order(type,size,crust,topping){
+//       this.type = type;
+//       this.size = size;
+//       this.crust = crust;
+//       this.topping = topping;
+//   }
 
-  Order.prototype.fullOrder = function(){
-      return this.type + " with a crust of " + this.crust + " and " + this.topping + " as toppings.";
+//   Order.prototype.fullOrder = function(){
+//       return this.type + " with a crust of " + this.crust + " and " + this.topping + " as toppings.";
 
-  };
+//   };
 
-  function Total(price,quantity,delivery){
-      this.price = price;
-      this.quantity = quantity;
-      this.delivery = delivery;
-  }
+//   function Total(price,quantity,delivery){
+//       this.price = price;
+//       this.quantity = quantity;
+//       this.delivery = delivery;
+//   }
 
-  Total.prototype.finalTotal = function(){
-      return this.price * this.quantity + this.delivery;
-  }
+//   Total.prototype.finalTotal = function(){
+//       return this.price * this.quantity + this.delivery;
+//   }
 
-  
+//   var sizePrice = [1000,800,600]
+//   var deliverPrice = [200];
+
+//   //User Interface Logic
+
+//   $(document).ready(function(){
+//       $('form#myForm').submit(function(event){
+//           event.PreventDefault();
+//           var pizzaType = $('type').val();
+//           var pizzaSize = parseInt($('#size').val());
+//           var pizzaCrust = $('#crust').val();
+//           var pizzaTop = $('#top').val();
+//           var pizzaQty = parseInt($('#qty').val());
+//           var pizzaPick = parseInt($('#pick').val());
+
+//           var price = sizePrice[pizzaSize - 1];
+
+//           var DeliveryCost = deliverPrices[pizzaPick - 1];
+
+//           newOrder = new Order(pizzaType,pizzaSize,pizzaCrust,pizzaTop);
+//           newTotal = new Total(price,pizzaQty,DeliveryCost);
+//           if (pizzaPick===1){
+//               alert("Your order is: " + newOrder.fullOrder() + " .Continue to see your total bill");
+//               alert("Your bill is: " + newTotal.finalTotal());
+//           }else{
+//               if(pizzaPick===2){
+//                   prompt("Enter where you want your pizza to be delivered");
+//                   alert("Your order has been received and it will be delivered.Continue to see your order details.");
+//                   alert("Your order is: " + newOrder.fullOrder() + ".continue to see your total bill");
+//                   alert("Your bill is: " + newTotal.finalTotal());
+//               }
+//           }
+//       });
+
+
+//sending message function after filling the contacts form
+
+$('form#contactForm').submit(function(event){
+    event.PreventDefault();
+    var name = $('#name').val();
+    var pass = $('#email').val();
+    var mess = $('#mess').val();
+    alert("Hi " + name + " We have received your message and we will get back to you.Thank you for contacting us");
+});
+
+//   });
+
