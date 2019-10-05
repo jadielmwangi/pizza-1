c4b35254a5a07a5bcb1744c22e530d9dcdb83831
margaret254 <maggiemwas91@gmail.com>
2019-10-05 19:03:14 +0300

    Create a function order in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
 $('.carousel').carousel({
     interval: 2000
   })
+
+  //Business logic
+  function Order(type,size,crust,topping){
+      this.type = type;
+      this.size = size;
+      this.crust = crust;
+      this.topping = topping;
+  }
+
+  Order.prototype.fullOrder = function(){
+      return this.type + " with a crust of " + this.crust + " and " + this.topping + " as toppings.";
+
+  };
+
+  function Total(price,quantity,delivery){
+      this.price = price;
+      this.quantity = quantity;
+      this.delivery = delivery;
+  }
+
+  Total.prototype.finalTotal = function(){
+      return this.price * this.quantity + this.delivery;
+  }
+
+  
